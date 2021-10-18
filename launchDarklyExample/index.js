c232ebf63da8af4f3c0575bd92c8ced512f95d16
Brian Rinaldi <brian.rinaldi@gmail.com>
2021-10-18 14:54:32 -0400

    small tweaks

--- launchDarklyExample/index.js
+++ launchDarklyExample/index.js
@@ -1,11 +1,13 @@
 const LaunchDarkly = require("launchdarkly-node-server-sdk");
-const client = LaunchDarkly.init("my-sdk-key");
+const client = LaunchDarkly.init("sdk-my-key");
 
 exports.handler = async (event) => {
   const request = event.Records[0].cf.request;
   const hostURL = request.headers.host[0].value;
   // all our files are under /site
   const URI = request.uri.replace("/site", "");
+  // remove the beta if they added it manually
+  const URI = request.uri.replace("/beta", "");
   await client.waitForInitialization();
   const landingPage = await client.variation(
     "rebrand",
