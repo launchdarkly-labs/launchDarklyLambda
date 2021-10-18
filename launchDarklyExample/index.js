042d49ca2ec4c0c88acb778bead9f7a446932708
Brian Rinaldi <brian.rinaldi@gmail.com>
2021-10-18 18:00:52 -0400

    fixes

--- launchDarklyExample/index.js
+++ launchDarklyExample/index.js
@@ -5,9 +5,9 @@ exports.handler = async (event) => {
   const request = event.Records[0].cf.request;
   const hostURL = request.headers.host[0].value;
   // all our files are under /site
-  const URI = request.uri.replace("/site", "");
+  let URI = request.uri.replace("/site", "");
   // remove the beta if they added it manually
-  const URI = request.uri.replace("/beta", "");
+  URI = request.uri.replace("/beta", "");
   await client.waitForInitialization();
   const landingPage = await client.variation(
     "rebrand",
