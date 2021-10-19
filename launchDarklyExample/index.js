8377c26a403b3342c8a970e51c904edeef0b9d38
Brian Rinaldi <brian.rinaldi@gmail.com>
2021-10-19 16:30:51 -0400

    refactoring

--- launchDarklyExample/index.js
+++ launchDarklyExample/index.js
@@ -1,30 +1,44 @@
 const LaunchDarkly = require("launchdarkly-node-server-sdk");
 const client = LaunchDarkly.init("sdk-my-sdk-key");
 
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
   const request = event.Records[0].cf.request;
-  const hostURL = request.headers.host[0].value;
-  // all our files are under /site
-  let URI = request.uri.replace("/site", "");
-  // remove the beta if they added it manually
-  URI = request.uri.replace("/beta", "");
+  const c = request.headers.host[0].value;
+
+  // initialize LaunchDarkly and get the flag value
   await client.waitForInitialization();
-  const landingPage = await client.variation(
+  const viewNewSite = await client.variation(
     "rebrand",
     { key: request.clientIp },
     false
   );
-  const URL = hostURL + landingPage + URI;
-  return {
-    status: "302",
-    statusDescription: "Found",
-    headers: {
-      location: [
-        {
-          key: "Location",
-          value: URL,
-        },
-      ],
-    },
-  };
+  console.log(`LaunchDarkly returned "${viewNewSite}"`);
+
+  // if they aren't being redirected to the new site just return them to whatever they requested
+  // or if they are already on the beta site
+  if (!viewNewSite || request.url.includes("/beta")) {
+    return callback(null, request);
+    // otherwise redirect them to the beta site
+  } else {
+    let URL =
+      "http://" +
+      hostname +
+      "/beta" +
+      request.uri.replace("/beta", "") +
+      (request.querystring === "" ? "" : "?" + request.querystring);
+    const response = {
+      status: "301",
+      statusDescription: "Found",
+      headers: {
+        location: [
+          {
+            key: "Location",
+            value: URL,
+          },
+        ],
+      },
+    };
+
+    callback(null, response);
+  }
 };
